961ef67049cfd554245cf5e2bde717cdd5ad0bb3
Ryan <rgwbarnes@gmail.com>
2014-01-20 20:16:22 -0800

    added address forms to user registrations and settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,13 @@ protected
  def configure_permitted_parameters
    devise_parameter_sanitizer.for(:sign_up) << :name 
    devise_parameter_sanitizer.for(:sign_up) << :phone
+   devise_parameter_sanitizer.for(:sign_up) << :address
+   devise_parameter_sanitizer.for(:sign_up) << :city
+   devise_parameter_sanitizer.for(:sign_up) << :zip
    devise_parameter_sanitizer.for(:account_update) << :name 
    devise_parameter_sanitizer.for(:account_update) << :phone
+   devise_parameter_sanitizer.for(:account_update) << :address
+   devise_parameter_sanitizer.for(:account_update) << :city
+   devise_parameter_sanitizer.for(:account_update) << :zip
  end
 end

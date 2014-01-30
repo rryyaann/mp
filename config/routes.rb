a9b5813acb532e99baeacbea9266cb56a26076da
Ryan <rgwbarnes@gmail.com>
2014-01-30 11:01:03 -0800

    finalized maid rating pins onto homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Mp::Application.routes.draw do
   resources :pins
 
   devise_for :users
-  root "pages#home"
+  root "pins#index"
   get "pricing" => "pages#pricing"
   get "where" => "pages#where"
   get "safety" => "pages#safety"
